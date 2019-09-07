de8fd34f4d42a4b67a66a8c3d8b8ec2c3a1d52a0
lifegpc <g1710431395@gmail.com>
2019-09-07 16:07:54 +0800

    Update

--- build.sh
+++ build.sh
@@ -11,4 +11,5 @@ g++ -O3 -o bin/help.o -c src/help.cpp
 g++ -O3 -o bin/string.o -c src/string.cpp
 g++ -O3 -o bin/json.o -c -w src/json.cpp
 g++ -O3 -o bin/convert/browserhistory.o -c -w src/convert/browserhistory.cpp
-g++ -O3 -o convert bin/main.o bin/help.o bin/string.o bin/json.o bin/convert/browserhistory.o -ljson-c
+g++ -O3 -o bin/file.o -c src/file.cpp
+g++ -O3 -o convert bin/main.o bin/help.o bin/string.o bin/json.o bin/convert/browserhistory.o bin/file.o -ljson-c
